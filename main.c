d994529975f88200b96b18d01021bf9a1a9e97a2
Bertrand-Rapello Baptiste <baptiste.bertrand-rapello@epitech.eu>
2015-05-27 14:37:30 +0200

    premier commit depuis mon PC EPITECH. first commit from my EPITECH's PC

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for  in /home/bbr2394/arm/helloWorld
+** 
+** Made by Bertrand-Rapello Baptiste
+** Login   <bertra_l@epitech.net>
+** 
+** Started on  Wed May 27 14:36:14 2015 Bertrand-Rapello Baptiste
+** Last update Wed May 27 14:36:53 2015 Bertrand-Rapello Baptiste
+*/
+
+#include <unistd.h>
+
+int	main()
+{
+  write(1, "Bonjour\n", 8);
+  return 0;
+}
